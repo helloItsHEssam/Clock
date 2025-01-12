5aa1bd5c4997f61496a62e4906420a2daafc19ca
Hessam Mahdiabadi <h.mahdiabadi@bluteam.ir>
2025-01-13 00:50:40 +0330

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by HEssam on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
